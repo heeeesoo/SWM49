1baa80b43d2d4fe412154198a5f32916bc911826
heesoo <11junghs@gmail.com>
2023-04-22 23:06:45 +0900

    feat : add side bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const StyledHeader = styled.div`
     display: flex;
     align-items: center;
+    justify-content: space-between;
     height: 70px;
+    padding: 10px;
     background: yellow;
 `
 
 const StyledTitle = styled(Link)`
     font-size: 30px;
-    padding: 10px;
     box-sizing: border-box;
     text-decoration: none;
 `
@@ -20,25 +23,62 @@ const StyledInfo = styled(Link)`
     text-decoration: none;
     padding: 5px;
 `
+
+const ButtonX = styled.div`
+
+`
+
+const SideBarWrap = styled.div`
+    z-index: 5;
+    padding: 12px;
+    border-radius: 15px 0 0 15px;
+    background-color: #e7e4e1;
+    height: 100%;
+    width: 80%;
+    right: -55%;
+    top: 0;
+    position: fixed;
+    transition: 0.5s ease;
+    &.open {
+        right: 0;
+        transition: 0.5s ease;
+    }
+`;
+
 const Header = () => {
-    const info = [0,1,2,3,4]
+    const [toggle, setToggle]=useState(false);
+    const info = [0,1,2,3,4];
+
+    const handleClick = () => {
+        setToggle(toggle => !toggle);
+    }
+
     return (
         <StyledHeader>
                 <StyledTitle to="/">
                     SWM 49팀 
                 </StyledTitle>
                 {
-                    info.map((v,i)=>{
-                        let url="info/"+i;
-                        return(
-                            <div key={v}>
-                            <StyledInfo to={url}>
-                                {i}
-                            </StyledInfo>
-                            </div>
-                        )
-                    })
+                    toggle &&
+                    <SideBarWrap>
+                        <ButtonX onClick={handleClick}>
+                            X
+                        </ButtonX>
+                        {
+                            info.map((v,i)=>{
+                                let url="info/"+i;
+                                return(
+                                    <div key={v}>
+                                    <StyledInfo to={url}>
+                                        {i}
+                                    </StyledInfo>
+                                    </div>
+                                )
+                            })
+                        }
+                    </SideBarWrap>
                 }
+                <FontAwesomeIcon onClick={handleClick} icon={faBars} size="2x"/>
         </StyledHeader>
     );
 };
--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import info0 from "../data/0.json";
 import info1 from "../data/1.json";
 import info2 from "../data/2.json";
 import info3 from "../data/3.json";
 import info4 from "../data/4.json";
 
+const StyledContainer = styled.div`
+    margin: 10px;
+`
+
+const StyledName = styled.div`
+    font-size: 20px;
+`
 const Info = () => {
     const { num } = useParams();
     const arr = [info0,info1,info2,info3,info4];
 
     return (
-        <div>
-            {arr[Number(num)].name}
+        <StyledContainer>
+            <StyledName>
+                {arr[Number(num)].name}
+            </StyledName>
             <br/>
-            {arr[Number(num)].text.split('\n').map(line => {
+            {arr[Number(num)].text.split('\n').map((line,i) => {
                 return (
-                    <span key={line}>{line}<br/></span>
+                    <span key={i}>{line}<br/></span>
                 )
             })}
-        </div>
+        </StyledContainer>
     );
 };
 
